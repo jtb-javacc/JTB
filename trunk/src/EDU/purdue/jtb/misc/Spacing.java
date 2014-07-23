1b9e3d6c3d8386a0a3718a658be0dcf422c7d01d
francisandre <francisandre@f399e9d4-1b24-e9a3-8d25-847b4d26a0b2>
2014-07-23 09:01:31 +0000

    restore updateSpc

--- trunk/src/EDU/purdue/jtb/misc/Spacing.java
+++ trunk/src/EDU/purdue/jtb/misc/Spacing.java
@@ -76,7 +76,7 @@ public class Spacing {
   public Spacing(final int indentAmt, final int aIndentLevel) {
     this(indentAmt);
     if (aIndentLevel > 0) {
-      update(aIndentLevel);
+      updateSpc(aIndentLevel);
     }
   }
 
@@ -101,7 +101,7 @@ public class Spacing {
    * 
    * @param numIndentLvls - the (positive or negative) indentation level delta.
    */
-  public void update(final int numIndentLvls) {
+  public void updateSpc(final int numIndentLvls) {
     indentLevel += numIndentLvls;
     if (indentLevel < 0)
       indentLevel = 0;
