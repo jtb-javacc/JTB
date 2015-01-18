cb489a33c34e2a334fa3cc020447294b2e46ea7e
marc.mazas@sopragroup.com <marc.mazas@sopragroup.com@f399e9d4-1b24-e9a3-8d25-847b4d26a0b2>
2015-01-18 18:03:34 +0000

    Error in tagging

--- tags/v1_4_9/v1_4_8/v1_4_7/src/EDU/purdue/jtb/misc/Language.java
+++
@@ -1,6 +0,0 @@
-package EDU.purdue.jtb.misc;
-
-
-public enum Language {
-  java, cpp
-}
--- tags/v1_4_9/v1_4_8/v1_4_7/src/EDU/purdue/jtb/misc/VisitorClass.java
+++
@@ -1,9 +0,0 @@
-package EDU.purdue.jtb.misc;
-
-
-@SuppressWarnings("javadoc")
-public interface VisitorClass {
-  String getClassName();
-  IVisitorClass getInterface();
-  String getOutfileName();
-}
