c3a127413a13e635936d5de94d7fd7a912e59893
marc.mazas@sopragroup.com <marc.mazas@sopragroup.com@f399e9d4-1b24-e9a3-8d25-847b4d26a0b2>
2015-01-18 18:00:05 +0000

    Changes for version 1.4.9 : fixed a regression in Annotator

--- trunk/src/EDU/purdue/jtb/misc/Globals.java
+++ trunk/src/EDU/purdue/jtb/misc/Globals.java
@@ -82,9 +82,9 @@ public class Globals {
   /** Turns on / off debugging class comment printing */
   public static final boolean DEBUG_FIELD_AND_SUB_COMMENTS = false;
   /** The program version */
-  public static final String  VERSION                      = "1.4.9";
+  public static final String  VERSION                      = "1.4.10";
   /** The serial uid version */
-  public static final long    SERIAL_UID                   = 149;
+  public static final long    SERIAL_UID                   = 1410;
   /** The java program name */
   public static final String  PROG_NAME                    = "JTB";
   /** The shell script name */
